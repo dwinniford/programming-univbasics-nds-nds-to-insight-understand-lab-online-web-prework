29cce3987db3287b0da47238235efa7270a6907d
darling-flag-8189 <dwinniford@gmail.com>
2019-12-13 21:20:02 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -13,7 +13,7 @@ def print_first_directors_movie_titles
   index = 0 
   movies_string = ""
   while index < directors_database[0][:movies].count do 
-    p directors_database[0][:movies][index][:title]
+    print directors_database[0][:movies][index][:title]
     index += 1 
   end 
   
